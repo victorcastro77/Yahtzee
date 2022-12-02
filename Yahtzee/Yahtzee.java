70d73e49475b569b3e9f5030cff1dbb7c9163c6a
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-12-02 11:44:51 -0800

    yahtzee

--- Yahtzee/Yahtzee.java
+++ Yahtzee/Yahtzee.java
@@ -5,7 +5,6 @@ public class Yahtzee
     private Die6 die3;
     private Die6 die4;
     private Die6 die5;
-    private Die6 die6;
     private int n;
     private String diceValues;
     public Yahtzee() {
@@ -14,7 +13,6 @@ public class Yahtzee
         die3 = new Die6();
         die4 = new Die6();
         die5 = new Die6();
-        die6 = new Die6();
     }
     
     
@@ -39,23 +37,22 @@ public class Yahtzee
             die4.roll();
         }
         if (dieNumber == 5) {
-            die5.roll();
+            die5.roll(); 
         }
     }
-    public String toString() {
-        diceValues = "Dice values: " +die1.getValue()+ ", " +die2.getValue()+ ", " +die3.getValue()+ ", " +die4.getValue()+ ", " +die5.getValue();
-        return diceValues;
-    }
+    
     public String summarize() {
-        int[] count = {0,0,0,0,0,0};
+        int[] count = {0,0,0,0,0,0,0};
         count[die1.getValue()]++;
         count[die2.getValue()]++;
         count[die3.getValue()]++;
         count[die4.getValue()]++;
         count[die5.getValue()]++;
-        count[die6.getValue()]++;
-
-        return  ("1-"+count[1]+", 2-"+count[2]+", 3-"+count[3]+", 4-"+count[4]+", 5-"+count[5]+", 6-"+count[6]);
+        return ("1-" + count[1] + "; 2-" + count[2] + "; 3-" + count[3] + "; 4-" + count[4] + "; 5-" + count[5] + "; 6-" + count[6]);
     }
-    
+
+    public String toString() {
+        diceValues = "Dice values: " +die1.getValue()+ ", " +die2.getValue()+ ", " +die3.getValue()+ ", " +die4.getValue()+ ", " +die5.getValue();
+        return diceValues;
+    }
 }
